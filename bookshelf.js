4c1be729aa52b43402f4099b1cfaa057fd9537da
Sina Karimi <sina@inlight.com.au>
2015-07-11 17:22:17 +1000

    Added username/password config to bookshelf connection

--- bookshelf.js
+++ bookshelf.js
@@ -2,8 +2,8 @@ var knex = require('knex')({
   client: 'pg',
   connection: {
     host     : 'localhost',
-    user     : 'roland',
-    password : 'postgres',
+    user     : process.env.POSTGRES_DB_USER || 'roland',
+    password : process.env.POSTGRES_DB_PASSWORD || 'postgres',
     database : 'BitcoinReddit',
     charset  : 'utf8'
   }
