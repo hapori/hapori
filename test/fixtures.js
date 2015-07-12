85f9a2595e7eb332250d57b86c07c91ba0145760
Roland Sharp <rolandnsharp@gmail.com>
2015-07-12 16:59:02 +1000

    Add default user fixtures.

---
+++ test/fixtures.js
@@ -0,0 +1,16 @@
+var user = {
+  default: {
+    username: 'usertestuser',
+    email: 'usertestuser@test.com',
+    passwordHash: 'testpassword',
+    salt: 'salt',
+    balance: 1000,
+    key: 'some bitcoin key',
+    address: '15U4eEyfEET9GqTSF4JpFRHAD8YGpYLbCE',
+    joined: '1827369128'
+  }
+}
+
+module.exports = {
+  user: user
+}
