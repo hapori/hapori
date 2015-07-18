882acac8ca23d7e560a23805c34350dc24476f79
Roland Sharp <rolandnsharp@gmail.com>
2015-07-18 23:44:35 +1000

    clean

--- models/vote.js
+++ models/vote.js
@@ -1,34 +1,3 @@
 var bookshelf = require('hapori-bookshelf');
 var User = bookshelf.Model.extend({ tableName: 'votes' }, {});
 module.exports = User;
-
-
-/*
-
-var db = require('../db');
-var query = db.query;
-
-
-
-exports.create = function (vote, client) {
-    return db.insert('votes', vote, client)
-};
-
-
-exports.find = function (query, client) {
-    return db.find('votes', query,  client)
-};
-
-exports.findAll = function (client) {
-    return db.find('votes', {}, client)
-};
-
-exports.findById = function (id, client) {
-    return db.find('votes', { id:id },  client)
-};
-
-exports.findByPostId = function (postId, client) {
-    return db.find('votes', { postId:postId },  client)
-};
-
-*/
