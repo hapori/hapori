c3aa35203833021cfc7906bacf483c06a077d810
Roland Sharp <rolandnsharp@gmail.com>
2015-07-25 20:13:57 +1000

    fix heroku postgress connection string

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
-// require('dotenv').load();
+require('dotenv').load();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
