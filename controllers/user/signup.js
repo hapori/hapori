af2259b1a1135677763c3574b4aaf25a61a24319
Clemens Ley <ley.clemens@gmail.com>
2015-08-06 15:58:07 +0200

    minor

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -94,6 +94,7 @@ module.exports = function(req, res, next) {
     var passwordHash = crypto.createHash('sha256').update(salt + ':' + password).digest('base64');
 
 
+/*
     // make sure we get a post request to /deposit when there is a transaction to that address
     chain.createNotification({
         type: "address",
@@ -105,7 +106,7 @@ module.exports = function(req, res, next) {
             else console.log(resp)
         }
     )
-
+*/
 
 
 
